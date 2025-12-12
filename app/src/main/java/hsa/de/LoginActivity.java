da4693626c3db1c6dca01429b123f579e1c5e943
Max Stiasny <maxstiasny@gmail.com>
2025-12-12 15:34:42 +0100

    Login und Registrierung mit Firebase integiert

---
+++ app/src/main/java/hsa/de/LoginActivity.java
@@ -0,0 +1,69 @@
+package hsa.de;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText editTextEmail, editTextPassword;
+    Button buttonLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+
+    mAuth = FirebaseAuth.getInstance();
+    editTextEmail = findViewById(R.id.email);
+    editTextPassword = findViewById(R.id.password);
+    buttonLog = findViewById(R.id.btn_login);
+
+
+    buttonLog.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String email, password;
+            email = editTextEmail.getText().toString();
+            password = editTextPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Login Succesfull", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Authentication failed,", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    });
+}
+}
--- app/src/main/java/hsa/de/MainActivity.java
+++ app/src/main/java/hsa/de/MainActivity.java
@@ -2,39 +2,58 @@ package hsa.de;
 
 import android.os.Bundle;
 import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // Diese ZWEI ZEILEN NICHT ÄNDERN!
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // AB HIER EIGENER CODE
+        mAuth = FirebaseAuth.getInstance();
 
-        final Button LoginButton = findViewById(R.id.login_button);
-
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+        final Button LoginViewButton = findViewById(R.id.login_view_button);
+        LoginViewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                setContentView(R.layout.activity_login);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
         final Button RegisterButton = findViewById(R.id.register_button);
-
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
                 public void onClick(View view){
-                setContentView(R.layout.activity_register);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
+
     }
 }
