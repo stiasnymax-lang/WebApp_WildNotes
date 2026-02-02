c507e31f06366bb62c2a2ac6fba226b181843c92
Max Stiasny <stiasnymax@gmail.com>
2026-02-02 21:17:16 +0100

    Code von Home_activity nach settings verschoben + RESET PASSWORD implementiert

--- app/src/main/java/hsa/de/SettingsActivity.java
+++ app/src/main/java/hsa/de/SettingsActivity.java
@@ -1,4 +1,72 @@
 package hsa.de;
 
-public class SettingsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button_logout;
+    Button button_reset;
+    TextView textView;
+    FirebaseUser user;
+    EditText new_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        auth = FirebaseAuth.getInstance();
+        button_logout = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        button_reset = findViewById(R.id.reset_password);
+
+
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user != null && user.getEmail() != null) {
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(user.getEmail());
+                    Toast.makeText(getApplicationContext(), "Password Reset E-Mail gesendet.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Keine E-Mail verfügbar.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+
+
 }
